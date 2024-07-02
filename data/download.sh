84560e0da00073b60add7893a99e6089ffa0b742
Jonathan Miller <jonathan.miller@tenefit.com>
2024-07-02 10:12:36 -1000

    Made changes to the downloader scripts

--- data/download.sh
+++ data/download.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-GFS_DATE="20161120"
-GFS_TIME="00"; # 00, 06, 12, 18
+GFS_DATE="20240701"
+GFS_TIME="12"; # 00, 06, 12, 18
 RES="1p00" # 0p25, 0p50 or 1p00
 BBOX="leftlon=0&rightlon=360&toplat=90&bottomlat=-90"
-LEVEL="lev_10_m_above_ground=on"
-GFS_URL="http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}.pl?file=gfs.t${GFS_TIME}z.pgrb2.${RES}.f000&${LEVEL}&${BBOX}&dir=%2Fgfs.${GFS_DATE}${GFS_TIME}"
+LEVEL="lev_1000_mb=on"
+FORECASTS=("f000" "f006" "f012" "f018" "f024" "f030" "f036")
 
-curl "${GFS_URL}&var_UGRD=on" -o utmp.grib
-curl "${GFS_URL}&var_VGRD=on" -o vtmp.grib
+# LEVEL="lev_20_m_above_ground=on"
+# LEVEL="lev_500_mb=on"
+# GFS_URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}.pl?file=gfs.t${GFS_TIME}z.pgrb2.${RES}.f000&${LEVEL}&${BBOX}&dir=%2Fgfs.${GFS_DATE}${GFS_TIME}"
+# GFS_URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}.pl?dir=%2Fgfs.${GFS_DATE}%2F${GFS_TIME}%2Fatmos&file=gfs.t${GFS_TIME}z.pgrb2.${RES}.anl&${LEVEL}&subregion=&toplat=90&leftlon=0&rightlon=360&bottomlat=-90"
+GFS_URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}.pl?dir=%2Fgfs.${GFS_DATE}%2F${GFS_TIME}%2Fatmos&${LEVEL}&subregion=&toplat=90&leftlon=0&rightlon=360&bottomlat=-90"
 
-grib_set -r -s packingType=grid_simple utmp.grib utmp.grib
-grib_set -r -s packingType=grid_simple vtmp.grib vtmp.grib
+# gfs.t18z.pgrb2.1p00.anl
+# gfs.t18z.pgrb2.1p00.f003
 
-printf "{\"u\":`grib_dump -j utmp.grib`,\"v\":`grib_dump -j vtmp.grib`}" > tmp.json
+for FORECAST in "${FORECASTS[@]}"
+do
 
-rm utmp.grib vtmp.grib
+  FILENAME="gfs.t${GFS_TIME}z.pgrb2.${RES}.${FORECAST}"
+  echo "Downloading GFS_URL: ${GFS_URL}&var_UGRD=on"
+  curl "${GFS_URL}&file=${FILENAME}&var_UGRD=on" -o utmp.grib
+  echo "Downloading GFS_URL: ${GFS_URL}&var_VGRD=on"
+  curl "${GFS_URL}&file=${FILENAME}&var_VGRD=on" -o vtmp.grib
 
-DIR=`dirname $0`
-node ${DIR}/prepare.js ${1}/${GFS_DATE}${GFS_TIME}
+  grib_set -r -s packingType=grid_simple utmp.grib utmp_processed.grib
+  grib_set -r -s packingType=grid_simple vtmp.grib vtmp_processed.grib
 
-rm tmp.json
+  printf "{\"u\":`grib_dump -j utmp_processed.grib`,\"v\":`grib_dump -j vtmp_processed.grib`}" > tmp.json
+
+  rm utmp.grib vtmp.grib utmp_processed.grib vtmp_processed.grib
+
+  DIR=`dirname $0`
+  node ${DIR}/prepare.js ${GFS_DATE}${GFS_TIME}"${FORECAST}"
+
+  rm tmp.json
+
+done
+
--- data/prepare.js
+++ data/prepare.js
@@ -3,11 +3,32 @@ const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('tmp.json'));
 const name = process.argv[2];
-const u = data.u;
-const v = data.v;
+const u = data["u"]["messages"][0];
+const v = data["v"]["messages"][0];
 
-const width = u.Ni;
-const height = u.Nj - 1;
+console.log(u);
+console.log(v);
+
+function getVariableValue(items, key) {
+    return items.filter(function (item) {
+        return item.key === key;
+    })[0].value;
+}
+
+const width = getVariableValue(u, 'Ni');
+const height = getVariableValue(u, "Nj") - 1;
+const uMinimum = getVariableValue(u, "minimum");
+const uMaximum = getVariableValue(u, "maximum");
+const vMinimum = getVariableValue(v, "minimum");
+const vMaximum = getVariableValue(v, "maximum");
+
+const uValues = getVariableValue(u, 'values');
+const vValues = getVariableValue(v, 'values');
+
+const dataDate = getVariableValue(u, 'dataDate');
+const dataTime = getVariableValue(u, 'dataTime');
+
+console.log(width, height);
 
 const png = new PNG({
     colorType: 2,
@@ -20,8 +41,8 @@ for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
         const i = (y * width + x) * 4;
         const k = y * width + (x + width / 2) % width;
-        png.data[i + 0] = Math.floor(255 * (u.values[k] - u.minimum) / (u.maximum - u.minimum));
-        png.data[i + 1] = Math.floor(255 * (v.values[k] - v.minimum) / (v.maximum - v.minimum));
+        png.data[i + 0] = Math.floor(255 * (uValues[k] - uMinimum) / (uMaximum - uMinimum));
+        png.data[i + 1] = Math.floor(255 * (vValues[k] - vMinimum) / (vMaximum - vMinimum));
         png.data[i + 2] = 0;
         png.data[i + 3] = 255;
     }
@@ -31,14 +52,69 @@ png.pack().pipe(fs.createWriteStream(name + '.png'));
 
 fs.writeFileSync(name + '.json', JSON.stringify({
     source: 'http://nomads.ncep.noaa.gov',
-    date: formatDate(u.dataDate + '', u.dataTime),
+    date: formatDate(dataDate + '', dataTime),
     width: width,
     height: height,
-    uMin: u.minimum,
-    uMax: u.maximum,
-    vMin: v.minimum,
-    vMax: v.maximum
+    uMin: uMinimum,
+    uMax: uMaximum,
+    vMin: vMinimum,
+    vMax: vMaximum
 }, null, 2) + '\n');
+
+// const MPS_TO_MPH = 2.23694;
+// const rgbPNG = PNG.sync.read(fs.readFileSync(name + '.png'));
+// const windSpeedPNG = new PNG({
+//     colorType: 2,
+//     filterType: 4,
+//     width: rgbPNG.width,
+//     height: rgbPNG.height
+// });
+
+// const maxVelocity = Math.sqrt(Math.pow(uMaximum, 2) + Math.pow(vMaximum, 2));
+// const maxVelocityMPH = maxVelocity * MPS_TO_MPH;
+// const radius = 1; // Adjust this value to change the amount of smoothing
+// const uValuesSmooth = movingAverage(uValues, radius);
+// const vValuesSmooth = movingAverage(vValues, radius);
+// const numPixels = uValuesSmooth.length;
+//
+// for (let i = 0; i < numPixels; i++) {
+//     const idx = i;
+//
+//     // Decode the u and v values
+//     const u = uValuesSmooth[idx];
+//     const v = vValuesSmooth[idx];
+//
+//     // Calculate the absolute velocity
+//     const velocity = Math.sqrt(Math.pow(u, 2) + Math.pow(v, 2));
+//     const velocityMPH = velocity * MPS_TO_MPH;
+//     // Normalize the velocity to the range [0, 255]
+//     // Encode the normalized velocity into the red channel
+//     windSpeedPNG.data[idx * 4] = Math.floor(255 * velocityMPH / maxVelocityMPH);
+//
+//     // Set the green and blue channels to 0
+//     windSpeedPNG.data[idx * 4 + 1] = 0;
+//     windSpeedPNG.data[idx * 4 + 2] = 0;
+//
+//     // Set the alpha channel to 255 (fully opaque)
+//     windSpeedPNG.data[idx * 4 + 3] = 255;
+//
+// }
+//
+// fs.writeFileSync(name + '_windSpeed.png', PNG.sync.write(windSpeedPNG));
+
+function movingAverage(array, radius) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        const start = Math.max(0, i - radius);
+        const end = Math.min(array.length - 1, i + radius);
+        let sum = 0;
+        for (let j = start; j <= end; j++) {
+            sum += array[j];
+        }
+        result[i] = sum / (end - start + 1);
+    }
+    return result;
+}
 
 function formatDate(date, time) {
     return date.substr(0, 4) + '-' + date.substr(4, 2) + '-' + date.substr(6, 2) + 'T' +
